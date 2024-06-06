2095fb65eab97062d225e730bb842060e8bdb122
DmitriiThe <>
2024-06-06 10:15:26 +0300

    making a handler for /users and function getUsers(), making and filling a users.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.message = 'OK';
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello World');
-    res.end();
+const fs = require("fs");
+
+const getUsers = () => {
+  return fs.readFileSync("./src/data/users.json");
+};
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/users") {
+    response.statusCode = 200;
+    response.message = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+
+    return;
+  }
+
+  response.statusCode = 200;
+  response.message = "OK";
+  response.header = "Content-Type: text/plain";
+  response.write("Hello World");
+  response.end();
 });
 
-server.listen(3003, '127.0.0.1', () => {
-    console.log('Server is running at http://127.0.0.1:3003/');
+server.listen(3003, "127.0.0.1", () => {
+  console.log("Server is running at http://127.0.0.1:3003/");
 });
-
